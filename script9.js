72356c002d035cd87f09d5e3499b81ec5deecce7
Mohammed-Muneef <muneefkorse@gmail.com>
2024-03-27 11:45:07 +0530

    exercise no 14 is incomplete

--- script9.js
+++ script9.js
@@ -1,21 +1,28 @@
 var arr = [1,4,3,2,5,7];
 
-var i = 4
-var right = i+1;
-var left = i-1;
-while(right<arr.length || left>=0)
-{
-    if(arr[i]<arr[right])
-    {
-        var rightGreat=arr[right];
-    }
-    if(arr[i]<arr[left])
-    {
-        var leftGreat=arr[left];
-    }
+var i = 4-1
 
-   
+
+
+for(var right = i+1; right<arr.length; right++)
+{
+    // console.log(right)
+    if(arr[right]>arr[i])
+    {
+        var greatright=arr[right];
+        // console.log(greatright)
+        break;
+    }
 }
 
-var great=Math.max(rightGreat,leftGreat)
-console.log(great);
+for(var left = i-1; left>=0 ;left--){
+    // console.log(left)
+    if(arr[left]> arr[i])
+    {
+        var greatleft=arr[left];
+        // console.log(greatleft)
+        break;
+    }
+}
+
+console.log(Math.max(greatleft,greatright))
